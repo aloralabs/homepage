a9c22c188d60e625b9ab2848b51cb1e4250ffc3a
Erik Berkun-Drevnig <erik.b22@gmail.com>
2020-04-10 05:39:44 -0400

    Use Hugo 0.69.0, use purgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,14 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: ['./hugo_stats.json'],
+    defaultExtractor: (content) => {
+        let els = JSON.parse(content).htmlElements
+        return els.tags.concat(els.classes, els.ids)
+    }
+})
+
 module.exports = {
     plugins: [
-        require('autoprefixer')
+        require('autoprefixer'),
+        purgecss
     ]
 }
